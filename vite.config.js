8cba39715caf7a85c0f78952c11e3a39680db749
Collin <76015895+SlothKami@users.noreply.github.com>
2024-04-02 23:29:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/slothkami.github.io/",
+  //base: "/slothkami.github.io/",
+  base: "./",
   plugins: [vue()],
 })
